97062cf0cb96babf91937233f680855fea5b36ee
my <my>
2018-12-01 20:48:40 +0800

    modify

--- trans/views.py
+++ trans/views.py
@@ -8,11 +8,14 @@ from django.conf.urls.static import static
 import codecs,random,os
 from trans.models import IMG
 from django.conf import settings
-
+import Enter
 def updata(request):
-    print(request)
     if request.FILES:
-        print('++++++'+request)
+        a = 0
+        try:
+            a = int(request.POST.get('idx'))
+        except Exception:
+            pass
         if not os.path.exists('upload'):
             os.makedirs('upload')
         imgname = str(request.FILES.get('fileUpload'))
@@ -23,11 +26,18 @@ def updata(request):
         imgout.write(request.FILES.get('fileUpload').read())
         #imgout = codecs.open(os.path.join('upload',imgname),'wb')
         #imgout.write(request.FILES.get('fileUpload').read())
-        readtmp = codecs.open('trans/templates/home.html','r','utf8').readlines()[0:10]
+        readtmp = codecs.open('trans/templates/home.html','r','utf8').readlines()[0:13]
         writetmp = codecs.open('trans/templates/home.html','w','utf8')
         writetmp.write(''.join(readtmp))
-        writetmp.write("<img src="+'"'+os.path.join('media','upload',imgname)+'"'+'alt="imgname_sample" />'+'\n')
-        writetmp.write('</form>\n</body>\n</html>')
+        writetmp.write("<img src="+'"'+os.path.join('media','upload',imgname)+'"  width="70%"'+'alt="sample" />'+'\n')
+        writetmp.write('</form>\n')
+        sen = ''
+        if a==0:
+            sen = Enter.enter(os.path.join(settings.MEDIA_ROOT,'upload',imgname),0)
+        else:
+            sen = Enter.enter(os.path.join(settings.MEDIA_ROOT,'upload',imgname),1)
+        writetmp.write(sen)
+        writetmp.write('</body>\n</html>')
         writetmp.close()
     #return render(request,'home.html')
     return HttpResponseRedirect(reverse('ho'))
